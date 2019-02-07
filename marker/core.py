d56768a41a49ebb95aa135a9530f24bb2e081613
Alejandro Gallo <aamsgallo@gmail.com>
2019-02-07 18:31:57 +0100

    Clean up variables

--- marker/core.py
+++ marker/core.py
@@ -16,18 +16,17 @@ else:
 
 
 def pick(search, options):
-    lines = [line.Line(opt, str(i)) for i, opt in enumerate(options)]
+    lines = [line.Line(opt, i) for i, opt in enumerate(options)]
     state = State(lines, search)
     # draw the screen (prompt + matchd marks)
     renderer.refresh(state)
     # wait for user input(returns selected mark)
-    output = read_line(state)
+    rline = read_line(state)
     # clear the screen
     renderer.erase()
-    if not output:
+    if not rline:
         return state.input
-    print(output)
-    return output.header
+    return (rline.index, rline.header)
 
 
 def read_line(state):
