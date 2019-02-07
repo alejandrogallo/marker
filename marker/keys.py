5ef45b0d49dc78e6bdc5e97e0acd2e0a8d3b3088
Alejandro Gallo <aamsgallo@gmail.com>
2019-02-07 18:52:55 +0100

    Clean up comments

--- marker/keys.py
+++ marker/keys.py
@@ -1,10 +1,13 @@
-CTRL_C = 3         # Ctrl-c
-ENTER = 13         # Enter
-CTRL_U = 21        # Ctrl+u
-ESC = 27           # Escape
-BACKSPACE =  127   # Backspace
-TAB = 9            # Tab
-RIGHT = -1     	   # FAKE CODE to abstract away the fact that a multibyte string is needed to represent arrow keys 
-DOWN = -2          # same
-UP = -3			   # same
-LEFT = -4          # same
+CTRL_C = 3
+ENTER = 13
+CTRL_U = 21
+ESC = 27
+BACKSPACE =
+TAB = 9
+
+# FAKE CODE to abstract away the fact that a multibyte string is needed to
+# represent arrow keys
+RIGHT = -1
+DOWN = -2
+UP = -3
+LEFT = -4
